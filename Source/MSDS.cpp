ee95e73c3cf9d56ad26f91e2ea28a4a0d873b037
FlyingSoda <fwillbrewer@gmail.com>
2018-03-17 23:42:53 -0700

    trying the same shit again.

--- Source/MSDS.cpp
+++ Source/MSDS.cpp
@@ -11,7 +11,7 @@
 
 SpringString::SpringString(int numNodes, int simIt, float length, float k_val, float mass, float damping){
     nodeSpace = numNodes/length;
-    nodeMass = mass;
+    nodeMass = 1/mass;
     nodek = k_val;
     nodeDamp = damping;
     nodeNum = numNodes;
@@ -31,54 +31,59 @@ SpringString::~SpringString() {
 
 void SpringString::setSampleRate(double SR){
     SRateStep = 1/SR; //Time step of Oscillator
+    SRateRK = SRateStep/6;
     nodeString[0].xvel = 5;
     nodeString[1].xpos = 1;
     nodeString[2].xpos = -1;
-    nodeString[10].xpos = 1;
 }
 
 //Does mass position calculation by iterating mathematics per node
 float SpringString::calcMovement(){
-
-    for(int j = 0; j < nodeIt; j++){
-        for (int i = 0; i < nodeNum; i++){
-            makeOld(nodeString[i]);
+    switch (method) {
+        case Euler: {
+            for(int j = 0; j < nodeIt; j++){
+                for (int i = 0; i < nodeNum; i++){
+                    makeOld(nodeString[i]);
+                }
+                //full body calculation of every Node
+                
+                //Operations on every node except beginning
+                for(int i = 1; i < nodeNum-1; i++){
+                    
+                    float force = -(2*nodeString[i].xpos - nodeString[i-1].oldxpos - nodeString[i+1].xpos) * nodek;
+                    float xaccel = force * nodeMass; //finds new acceleration for oscillator
+                    
+                    //sets new nodeString[i] velocity to new veloctiy from acceleration and
+                    //sets and sends new nodeString[i] position using velocity
+                    nodeString[i].xvel = euler(nodeString[i].xvel, xaccel/nodeIt);
+                    nodeString[i].xpos = euler(nodeString[i].xpos, nodeString[i].xvel/nodeIt);
+                }
+                
+                //Operations on only end node
+                float lnxaccel = (nodeString[nodeNum-2].oldxpos - nodeString[nodeNum-1].xpos) * nodek - nodeString[nodeNum-1].oldxvel * nodeDamp;
+                lnxaccel *= nodeMass;
+                //set nodes for next passthrough
+                nodeString[nodeNum-1].xvel = euler(nodeString[nodeNum-1].xvel, lnxaccel / nodeIt);
+                nodeString[nodeNum-1].xpos = euler(nodeString[nodeNum-1].xpos, nodeString[nodeNum-1].xvel / nodeIt);
+            }
+            break;
         }
-        //full body calculation of every Node
-        
-                                //  calculation of single node
-                                //    float accel = -(nodeString[10].xpos) * nodek;//- nodeString[nodeNum-1].oldxvel * nodeDamp;
-                                //    accel /= nodeMass;
-                                //
-                                //    //set nodes for next passthrough
-                                //    nodeString[10].xvel = euler(nodeString[10].xvel, accel);
-                                //    nodeString[10].xpos = euler(nodeString[10].xpos, nodeString[10].xvel);
-                                //
-                                //
-        
-
-
-        
-        //Operations on every node except beginning and end
-        for(int i = 1; i < nodeNum-1; i++){
             
-            float force = -(2*nodeString[i].xpos - nodeString[i-1].oldxpos - nodeString[i+1].xpos) * nodek;
-            float xaccel = force / nodeMass; //finds new acceleration for oscillator
+        case RungeKutta4:{
             
-            //sets new nodeString[i] velocity to new veloctiy from acceleration and
-            //sets and sends new nodeString[i] position using velocity
-            nodeString[i].xvel = euler(nodeString[i].xvel, xaccel/nodeIt);
-            nodeString[i].xpos = euler(nodeString[i].xpos, nodeString[i].xvel/nodeIt);
+            for (int i = 0; i < nodeNum; i++){
+                makeOld(nodeString[i]);
+            }
+            rK4(nodeString, 1);
+            break;
         }
-        
-        //Operations on only end node
-        float lnxaccel = (nodeString[nodeNum-2].oldxpos - nodeString[nodeNum-1].xpos) * nodek ;//- nodeString[nodeNum-1].oldxvel * nodeDamp;
-        lnxaccel /= nodeMass;
-        //set nodes for next passthrough
-        nodeString[nodeNum-1].xvel = euler(nodeString[nodeNum-1].xvel, lnxaccel / nodeIt);
-        nodeString[nodeNum-1].xpos = euler(nodeString[nodeNum-1].xpos, nodeString[nodeNum-1].xvel / nodeIt);
+            
+        default:
+            std::cout << "Method not determined" << std::endl;
+            break;
     }
-    //std::cout << nodeString[0].xpos << " " << nodeString[1].xpos << " " << nodeString[2].xpos << std::endl;
+    
+    std::cout << nodeString[0].xpos << " " << nodeString[1].xpos << " " << nodeString[2].xpos << std::endl;
     
     return nodeString[1].xpos;
 }
@@ -88,8 +93,90 @@ float SpringString::euler(float initval, float derval){
     return initval + SRateStep * derval; //previous position plus the velocity * 1/SampleRate (time)
 }
 
-float SpringString::rK4(node &n){
+float SpringString::rK4(node n[], int position){
+    
+    //Sends work to each helper function
+    rK4p1(n, position);
+    rK4p2(n, position);
+    rK4p3(n, position);
+    rK4p4(n, position);
+    rK4f(n, position);
+
     return 0;
+}
+
+void SpringString::rK4p1(node n[], int position){
+    //    x1 = x
+    //    v1 = v
+    //    a1 = a(x1, v1)
+    
+    n[position].vel[0] = n[position].xvel;
+    n[position].pos[0] = n[position].xpos;
+    n[position].acc[0] = -(2*n[position].xpos - n[position-1].oldxpos - n[position+1].xpos) * nodek - nodeString[position].oldxvel * nodeDamp;
+    n[position].acc[0] *= nodeMass;
+    
+    if(position < nodeNum-1){
+        rK4p1(n, position + 1);
+    }
+    
+}
+
+void SpringString::rK4p2(node n[], int position){
+    //    x2 = x + 0.5*v1*dt
+    //    v2 = v + 0.5*a1*dt
+    //    a2 = a(x2, v2)
+    
+    n[position].pos[1] = euler(n[position].xpos, n[position].xvel * .5);
+    n[position].vel[1] = euler(n[position].xvel, n[position].acc[0] * .5);
+    
+    if(position < nodeNum-1){
+        rK4p2(n, position + 1);
+    }
+    
+    n[position].acc[1] = -(2*n[position].pos[1] - n[position-1].pos[1] - n[position+1].pos[1]) * nodek - n[position].vel[1] * nodeDamp;
+    n[position].acc[1] *= nodeMass;
+}
+
+void SpringString::rK4p3(node n[], int position){
+    //    x3 = x + 0.5*v2*dt
+    //    v3 = v + 0.5*a2*dt
+    //    a3 = a(x3, v3)
+    
+    n[position].pos[2] = euler(n[position].pos[1], n[position].vel[1] * .5);
+    n[position].vel[2] = euler(n[position].vel[1], n[position].acc[1] * .5);
+    
+    if(position < nodeNum-1){
+        rK4p3(n, position + 1);
+    }
+    
+    n[position].acc[2] = -(2*n[position].pos[2] - n[position-1].pos[2] - n[position+1].pos[2]) * nodek - n[position].vel[2] * nodeDamp;
+    n[position].acc[2] *= nodeMass;
+}
+void SpringString::rK4p4(node n[], int position){
+    //    x4 = x + v3*dt
+    //    v4 = v + a3*dt
+    //    a4 = a(x4, v4)
+    
+    n[position].pos[3] = euler(n[position].pos[2], n[position].vel[2]);
+    n[position].vel[3] = euler(n[position].vel[2], n[position].acc[2]);
+    
+    if(position < nodeNum-1){
+        rK4p4(n, position + 1);
+    }
+    
+    n[position].acc[3] = -(2*n[position].pos[3] - n[position-1].pos[3] - n[position+1].pos[3]) * nodek - n[position].vel[3] * nodeDamp;
+    n[position].acc[3] *= nodeMass;
+}
+void SpringString::rK4f(node n[], int position){
+    //    xf = x + (dt/6.0)*(v1 + 2*v2 + 2*v3 + v4)
+    //    vf = v + (dt/6.0)*(a1 + 2*a2 + 2*a3 + a4)
+    
+    n[position].xpos = n[position].xpos + SRateRK * (n[position].vel[0] + 2*n[position].vel[1] + 2*n[position].vel[2] + n[position].vel[3]);
+    n[position].xvel = n[position].xvel + SRateRK * (n[position].acc[0] + 2*n[position].acc[1] + 2*n[position].acc[2] + n[position].acc[3]);
+    
+    if(position < nodeNum-1){
+        rK4f(n, position + 1);
+    }
 }
 
 void SpringString::makeOld(node& handle){
--- Source/MSDS.h
+++ Source/MSDS.h
@@ -34,6 +34,11 @@ struct node
     float zpos = 0;
     float zvel = 0;
     
+    //variables for Runge Kutta
+    float pos[4] = {0, 0, 0, 0};
+    float vel[4] = {0, 0, 0, 0};
+    float acc[4] = {0, 0, 0, 0};
+    
     //past position
     float oldxpos = 0;
     float oldxvel = 0;
@@ -53,7 +58,8 @@ public:
     //Node distance from each other is length/numNodes.
     //k_val represents spring constant 'k' in F = ma = -kx
     //mass represents individual mass of nodes
-    SpringString(int numNodes = 10, int simIt = 100, float length = 10.f, float k_val = 10000000.f, float mass = 1.f, float damping = 10.f);
+    //numNodes MUST be at least 2, to have an anchor
+    SpringString(int numNodes = 7, int simIt = 100, float length = 10.f, float k_val = 1000000.f, float mass = 1.f, float damping = 0.f);
     ~SpringString();
     
     void setSampleRate(double SR);
@@ -62,21 +68,30 @@ public:
 private:
     node nodeString[100];
     float SRateStep;    // 1/SampleRate, used to determine iteration speed.
+    float SRateRK;      // 1/SampleRate/6, to determine Runge-Kutta
     float nodeSpace;    //Distance between each Node
-    float nodeMass;     //Mass of each Node
+    float nodeMass;     // 1/Mass of each Node
     float nodek;        //K value (tension value) of Node
     float nodeDamp;     //Rate that Node loses energy)
     int nodeNum;        //Number of Nodes
     int nodeIt;         //number of calcMovements called before returning position of indexed node
-
+    
+    enum Method {Euler, RungeKutta4}; //Determines whether to calculate using Euler or Runge Kutta method
+    Method method = RungeKutta4;
+    
     int randcount = 0;  //debugging counters
     int randcount2 = 0; //
     
     //Calculates Node position using Euler's method
     float euler(float initval, float derval);
     
-    //Calculates Node position using Runge Kutta method
-    float rK4(node &n);
+    //Calculates Node position using Runge Kutta method with helper functions p1-p4
+    float rK4(node n[], int position);
+    void rK4p1(node n[], int position);
+    void rK4p2(node n[], int position);
+    void rK4p3(node n[], int position);
+    void rK4p4(node n[], int position);
+    void rK4f(node n[], int position);
 
     //Pushes node values back
     void makeOld(node& handle);
